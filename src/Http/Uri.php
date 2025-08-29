c9b654ea35a66452d91895816ae9d0d1f4a8df8e
Jack Jefferson <jack.jefferson@mtc.co.uk>
2025-08-29 12:53:37 +0100

    Built constructor and helpers for Uri.
    The URI object now has a constructor and some helpers to build the
    object. The goal was to keep this safe and clean.

--- src/Http/Uri.php
+++ src/Http/Uri.php
@@ -7,6 +7,63 @@ use Psr\Http\Message\UriInterface;
 
 class Uri implements UriInterface
 {
+    private $scheme;
+    private $authority;
+    private $userInfo;
+    private $host;
+    private $port;
+    private $path;
+    private $query;
+    private $fragment;
+
+    private function buildUserInfo($user = '', $pass = null) {
+        $this->userInfo = $user;
+        if (isset($pass) && $this->userInfo !== '') {
+            $this->userInfo .= ':' . $pass;
+        }
+    }
+
+    private function buildAuthority($host = '') {
+        if ($host) {
+            $this->authority = ($this->userInfo ? $this->userInfo . '@' : '') . $host;
+            if ($this->port) {
+                $this->authority .= ':' . $this->port;
+            }
+        }
+    }
+
+    public function __construct($uri)
+    {
+        $parts = parse_url($uri);
+
+        if (isset($parts['scheme'])) {
+            $this->scheme = $parts['scheme'];
+        }
+
+        $this->buildUserInfo($parts['user'] ?? '', $parts['pass'] ?? null);
+
+        if (isset($parts['host'])) {
+            $this->host = $parts['host'];
+        }
+
+        if (isset($parts['port'])) {
+            $this->port = $parts['port'];
+        }
+
+        $this->buildAuthority($parts['host']);
+
+        if (isset($parts['path'])) {
+            $this->path = $parts['path'];
+        }
+
+        if (isset($parts['query'])) {
+            $this->query = $parts['query'];
+        }
+
+        if (isset($parts['fragment'])) {
+            $this->fragment = $parts['fragment'];
+        }
+    }
 
     public function getScheme(): string
     {
@@ -50,37 +107,44 @@ class Uri implements UriInterface
 
     public function withScheme(string $scheme): UriInterface
     {
-        return new Uri();
+        $clone = clone $this;
+        return $clone;
     }
 
     public function withUserInfo(string $user, ?string $password = null): UriInterface
     {
-        return new Uri();
+        $clone = clone $this;
+        return $clone;
     }
 
     public function withHost(string $host): UriInterface
     {
-        return new Uri();
+        $clone = clone $this;
+        return $clone;
     }
 
     public function withPort(?int $port): UriInterface
     {
-        return new Uri();
+        $clone = clone $this;
+        return $clone;
     }
 
     public function withPath(string $path): UriInterface
     {
-        return new Uri();
+        $clone = clone $this;
+        return $clone;
     }
 
     public function withQuery(string $query): UriInterface
     {
-        return new Uri();
+        $clone = clone $this;
+        return $clone;
     }
 
     public function withFragment(string $fragment): UriInterface
     {
-        return new Uri();
+        $clone = clone $this;
+        return $clone;
     }
 
     public function __toString(): string
